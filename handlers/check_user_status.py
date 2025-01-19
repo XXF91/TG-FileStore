25ed8e383103dad456c450f8b03f2988a76a598c
XXF91 <mrsjoaker@gmail.com>
2025-01-19 15:33:38 +0200

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -1,4 +1,4 @@
-# (c) Mr. Vishal & @AbirHasan2005 @PredatorHackerzZ
+
 
 import datetime
 from configs import Config
@@ -23,6 +23,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @TeleRoid14 😝", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact @X_XF8 😝", quote=True)
             return
     await cmd.continue_propagation()
