e99154065aaf37a1a1d52b1b667dcb67ae8e6f0f
XXF91 <mrsjoaker@gmail.com>
2025-01-24 01:53:19 +0200

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005 | @PredatorHackerzZ | @MrAbhi2k3
+# (c) @AbirHasan2005 | @X_XF8 | @MrAbhi2k3
 
 import asyncio
 import requests
@@ -70,7 +70,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(SaveMessage.id))}"
+        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=X_XF8_{str_to_b64(str(SaveMessage.id))}"
         short_link = get_short(share_link)
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {short_link} \n\n"
@@ -114,7 +114,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(file_er_id)}"
+        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=X_XF8_{str_to_b64(file_er_id)}"
         short_link = get_short(share_link)
         await editable.edit(
             "**Your File Stored in my Database!**\n\n"
