22c12725d0a13ec166799fee0e4338bbf4aee9aa
XXF91 <mrsjoaker@gmail.com>
2025-01-24 01:59:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -390,7 +390,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                                InlineKeyboardButton("🤖 انضم الي القناة", url=invite_link.invite_link)
                             ],
                             [
                                 InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
